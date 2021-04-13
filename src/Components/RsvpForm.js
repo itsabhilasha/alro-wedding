7aa7f29f9c24820fa240ced49ba41540d1a0bc2a
Rohit Gaur <rohitgaur7@gmail.com>
2021-04-13 21:47:55 +0530

    update env

--- src/Components/RsvpForm.js
+++ src/Components/RsvpForm.js
@@ -29,32 +29,29 @@ function RsvpForm({ visible, toggleRsvp }) {
         brideGroom: brideGroom,
       };
 
-      emailjs
-        .send(
-          process.env.REACT_APP_SERVICE_ID,
-          process.env.REACT_APP_TEMPLATE_ID,
-          templateParams,
-          process.env.REACT_APP_USER_ID
-        )
-        .then(
-          (result) => {
-            setWaiting(false);
-            setName("");
-            setDate("");
-            setBrideGroom("");
+      const serviceID = process.env.REACT_APP_SERVICE_ID;
+      const templateID = process.env.REACT_APP_TEMPLATE_ID;
+      const userID = process.env.REACT_APP_USER_ID;
 
-            if (result.text === "OK") {
-              alert("RSVP Successfull!");
-              toggleRsvp();
-            }
-          },
-          (error) => {
-            setWaiting(false);
-            alert(
-              "Error: Please try again or contact site admin if this persists."
-            );
+      emailjs.send(serviceID, templateID, templateParams, userID).then(
+        (result) => {
+          setWaiting(false);
+          setName("");
+          setDate("");
+          setBrideGroom("");
+
+          if (result.text === "OK") {
+            alert("RSVP Successfull!");
+            toggleRsvp();
           }
-        );
+        },
+        (error) => {
+          setWaiting(false);
+          alert(
+            "Error: Please try again or contact site admin if this persists."
+          );
+        }
+      );
     }
   };
 
